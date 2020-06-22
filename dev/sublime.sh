1e572a67c31b8a7a95cc1dfd19b40b11ee94dac1
Cristian Pop <cristian.pop@sectorlabs.ro>
2020-06-22 12:57:47 +0300

    fix sublime installer

--- dev/sublime.sh
+++ dev/sublime.sh
@@ -7,4 +7,4 @@ mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
 curl "https://packagecontrol.io/Package%20Control.sublime-package" > ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
 
 # Create symlink to subl command
-ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+#ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
