77f5130eb45550dec9bbb3104b7bfd4073032b5b
cristian.pop <crisp2u@gmail.com>
2024-05-09 22:15:47 +0300

    cleanup

--- dev/env.zsh
+++ dev/env.zsh
@@ -4,7 +4,6 @@ export EDITOR='subl -w'
 export NODE_OPTIONS=--max_old_space_size=4096
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
-export ALGOLIA_ADMIN_API_KEY=donotindex
 export PATH="/usr/local/opt/postgresql/bin:$PATH"
 export PATH="/usr/local/opt/node@8/bin:$PATH"
 export PATH="$HOME/Library/Python/3.7/bin:$PATH"
