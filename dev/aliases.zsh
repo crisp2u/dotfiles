ee6b99448e651919155d866dad5a295545727636
cristian.pop <crisp2u@gmail.com>
2024-08-22 01:43:12 +0300

    updates

--- dev/aliases.zsh
+++ dev/aliases.zsh
@@ -1,6 +1,5 @@
-alias ls='exa'
+alias ls='eza'
 alias dir='ls -l'
 
-alias hubps='hub pull-request -b staging -m "Deploy to staging"'
 alias curltime="curl -w '\ntime_namelookup:  %{time_namelookup}\n       time_connect:  %{time_connect}\n    time_appconnect:  %{time_appconnect}\n   time_pretransfer:  %{time_pretransfer}\n      time_redirect:  %{time_redirect}\n time_starttransfer:  %{time_starttransfer}\n                    ----------\n         time_total:  %{time_total}\n' \"$@\" "
-alias curlh="curl -s -o /dev/null --dump-header - \"$@\""alias hubps='hub pull-request -b staging -m "Deploy to staging"'
+alias curlh="curl -s -o /dev/null --dump-header - \"$@\""
--- sl/aliases.zsh
+++ sl/aliases.zsh
@@ -1,7 +1,1 @@
-alias ls='exa'
-alias dir='ls -l'
-
 alias hubps='hub pull-request -b staging -m "Deploy to staging"'
-
-alias curltime="curl -w '\ntime_namelookup:  %{time_namelookup}\n       time_connect:  %{time_connect}\n    time_appconnect:  %{time_appconnect}\n   time_pretransfer:  %{time_pretransfer}\n      time_redirect:  %{time_redirect}\n time_starttransfer:  %{time_starttransfer}\n                    ----------\n         time_total:  %{time_total}\n' \"$@\" "
-alias curlh="curl -s -o /dev/null --dump-header - \"$@\""
