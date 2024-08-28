e99dda0d3fe6e049da0e11dfe4e77219e4d2dc76
cristian.pop <crisp2u@gmail.com>
2024-08-28 18:34:48 +0300

    install awscli manually to remove brew python11 dep

--- dev/install.sh
+++ dev/install.sh
@@ -7,3 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #sed -i .bak 's/2048/10240/g' /Users/`id -un`/Library/Group\ Containers/group.com.docker/settings.json
 
 $DIR/sublime.sh
+
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+installer -pkg AWSCLIV2.pkg -target /
+rm AWSCLIV2.pkg
